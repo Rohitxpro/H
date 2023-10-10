acb82e20b10e81547be14c453b0839ba33dfd8e4
Rohitxpro <140487047+Rohitxpro@users.noreply.github.com>
2023-10-10 09:06:40 +0530

    Update vars.py

--- AdityaHalder/modules/vars.py
+++ AdityaHalder/modules/vars.py
@@ -11,7 +11,7 @@ if os.path.exists("Internal"):
 
 class Config(object):
     # REQUIRED VARIABLES
-    API_ID = int(getenv("API_ID", 11107466))
+    API_ID = int(getenv("API_ID",11107466))
     API_HASH = getenv("API_HASH", "303837af39dfd53ff9b60a56f6ca3bc6")
     BOT_TOKEN = getenv("BOT_TOKEN", "6652949143:AAFmXmYdM484jgo217X8CPxwNHmd4M7zbxk")
     STRING_SESSION = getenv("STRING_SESSION", "BQCpfIoAxv3PTQ6Hpf9uFesBOIjcmDghd6As2qpfqrCTN_Pktvzd5HkrBoc5mQmCP34wRK_P783D9_7FLGMmDaeIICvZ47Vmu_bU43iCwv678sUlmfDUvS8uA_SU3cdr3mO0CvSPB5ZnArSbYjyNeOXiG_yKUXxIuso1BiYT8suXQGcrfhmu8XDiZADY_6YUTbHuchYj6aHfeM0Vq3nLYLNrDzqXVGo-ggvFIGqBtDlmCbtwaxzeluDLL0KpEzFvLaNgibj5pAPSFkXcAaPt6xc-27ZQY1eEYMDod_o477kcvQU_D44PPIPMfYKqm0Qxnpj5sRRlMVU2j32hWbHlsqSFvkb5xAAAAAGEsRkqAA")
